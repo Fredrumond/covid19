2ae2d714173e5656c403f5102590cdf975528839
Frederico <fredrumond@gmail.com>
2020-07-30 16:13:09 -0300

    Layout base home

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,1 +1,82 @@
-Oi home
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COVID-19</title>
+    <link rel="stylesheet" href="css/reset.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+    <nav class="menu">
+        <div class="logo">
+            <h1>Painel COVID-<span>19</span></h1>
+        </div>
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="#" class="button">
+                        Entrar
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="highlights">
+            <div class="highlights-header">
+                <div>
+                    <h1>Casos no Brasil</h1>
+                    <small>Atualizado em: 29/07/2020 19:30</small>
+                </div>
+                <div>
+                    <button class="button">Faça já seu registro</button>
+                </div>
+            </div>
+            <div class="boxes">
+                <div class="box warning">
+                    <div class="title">Casos ativos</div>
+                    <div class="count">736473647</div>
+                </div>
+                <div class="box success">
+                    <div class="title">Casos recuperados</div>
+                    <div class="count">736473647</div>
+                </div>
+                <div class="box info">
+                    <div class="title">Casos confirmados</div>
+                    <div class="count">736473647</div>
+                </div>
+                <div class="box danger">
+                    <div class="title">Óbitos confirmados</div>
+                    <div class="count">736473647</div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="informations">
+            <div class="card">
+                <div class="content-img">
+                    <img src="img/undraw_medical_care_movn.png" alt="">
+                </div>
+            </div>
+            <div class="card">
+                <div class="content-img">
+                    <img src="img/undraw_medical_research_qg4d.png" alt="">
+                </div>
+            </div>
+            <div class="card">
+                <div class="content-img">
+                    <img src="img/undraw_coming_home_52ir.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="inner">
+          <p><a href="https://github.com/Fredrumond">Frederico Xavier Drumond</a></p>
+        </div>
+      </footer>
+</body>
+</html>
